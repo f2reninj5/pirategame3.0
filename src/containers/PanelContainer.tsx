bf453711af09bc5ae730e49b709e6463c3857883
f2reninj5 <64966099+f2reninj5@users.noreply.github.com>
2024-08-26 22:02:03 +0100

    Updated Panel

--- src/containers/PanelContainer.tsx
+++ src/containers/PanelContainer.tsx
@@ -2,7 +2,7 @@ import React, { type ReactNode } from 'react'
 
 function PanelContainer ({ children }: { children: ReactNode }): React.JSX.Element {
   return (
-    <div></div>
+    <div>{children}</div>
   )
 }
 
