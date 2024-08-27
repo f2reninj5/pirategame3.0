17027b8135e841903309eb4fb1b260abb74d1fc2
fn <64966099+f2reninj5@users.noreply.github.com>
2024-08-27 21:02:12 +0100

    Renamed Banner stylesheet file

--- src/containers/FooterContainer.tsx
+++ src/containers/FooterContainer.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './Banner.css'
+import './BannerContainer.css'
 
 function FooterContainer ({ children }: { children: React.ReactNode }): React.JSX.Element {
   return (
