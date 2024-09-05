bcf12b1f82757fdd50a3b8f5fe3a70c38168a9f9
f2reninj5 <64966099+f2reninj5@users.noreply.github.com>
2024-09-06 00:45:18 +0100

    Added functionality to GridHistory component

--- src/component/GridHistoryComponent.tsx
+++ src/component/GridHistoryComponent.tsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GridContext } from '../context/GridContext'
 
 function GridHistoryComponent (): React.ReactElement {
+  const [{ grid }] = useContext(GridContext)
+  const recentHistory = grid.history.slice(Math.max(0, grid.history.length - 6), grid.history.length).reverse()
+  const cells = []
+
+  for (let i = 0; i < recentHistory.length; i++) {
+    const cell = recentHistory[i]
+
+    cells.push(
+      <div key={i}>
+        <p>{cell.label}</p>
+      </div>
+    )
+  }
+
+  for (let i = recentHistory.length; i < 6; i++) {
+    cells.push(
+      <div key={i}>
+        <p>pad</p>
+      </div>
+    )
+  }
+
   return (
-    <></>
+    <div>
+      <h4>Recent Squares</h4>
+      {cells}
+    </div>
   )
 }
 
