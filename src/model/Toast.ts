120f67c1616512d1f6e15c1268e6e44de44c4ceb
f2reninj5 <64966099+f2reninj5@users.noreply.github.com>
2024-09-06 23:29:38 +0100

    Started Toast model

---
+++ src/model/Toast.ts
@@ -0,0 +1,25 @@
+import { ToastType } from '../context/ToastStackContext'
+
+export const ToastMessage: Record<ToastType, Record<string, string>> = {
+  [ToastType.Error]: {
+    Test1: 'test1'
+  },
+  [ToastType.Info]: {
+    Test2: 'test2'
+  },
+  [ToastType.Success]: {
+    Test3: 'test3'
+  }
+}
+
+export default class Toast<T extends ToastType> {
+  public type: T
+  public message: keyof typeof ToastMessage[T]
+  public count: number = 1
+  public timeout: ReturnType<typeof setTimeout> | undefined
+
+  constructor (type: T, message: keyof typeof ToastMessage[T]) {
+    this.type = type
+    this.message = message
+  }
+}
